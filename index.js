b4407b7fb207fa4d6897a3125b2bbf65f963ff12
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-01-31 01:01:32 +0700

    init Project

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const server = app.listen(
+  3000,
+  console.log("Socket.io Hello Wolrd server started")
+);
+app.use(express.static(path.join(__dirname, "public")));
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    transports: ["websocket", "polling"],
+    credentials: true,
+  },
+  allowEIO3: true,
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.send(
+    JSON.stringify({
+      type: "serverMessage",
+      message: "Welcome to the most interesting chat room on earth!",
+    })
+  );
+  socket.on('message', (message) => {
+    message = JSON.parse(message);
+    if (message.type == "userMessage") {
+        socket.broadcast.send(JSON.stringify(message));
+        message.type = "myMessage";
+        socket.send(JSON.stringify(message));
+    }
+  })
+});
