4f029dccdfca7a152b2f0751adebfde7814cc6c6
boomgt <32265715+champseo2017@users.noreply.github.com>
2022-02-23 08:35:07 +0700

    54:47
    https://www.youtube.com/watch?v=jD7FnbI76Hg

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
@@ -9,9 +12,15 @@ const socket = io();
 // Join chatroom
 socket.emit("joinRoom", { username, room });
 
+// Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
 // Message from server
 socket.on("message", (message) => {
-  console.log('message', message)
+  console.log("message", message);
   outputMessage(message);
   // Scroll down
   chatMessages.scrollTop = chatMessages.scrollHeight;
@@ -41,4 +50,9 @@ function outputMessage(message) {
     ${message.text}
   </p>`;
   document.querySelector(".chat-messages").appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room
 }
